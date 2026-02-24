5d7d9d3e8cb9cba2efdb29133624598acbc5d682
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-02-24 08:40:39 +0530

    24-02-26

--- ViPythonTutorial/vi_basics/vi_conditional_statements.py
+++ ViPythonTutorial/vi_basics/vi_conditional_statements.py
@@ -16,8 +16,29 @@ Syntax:
 Indentation: Leading spaces(spaces given at the beginning of any statement) given to define a block of code
 '''
 age = int(input("Please enter your age:"))
-
+'''
 if age >= 18:
     print("You're an adult")
 else:
     print("You're not an adult")
+'''
+if age > 0:
+    if age <= 3:
+        print("You're an infant")
+        
+    elif age <= 12:
+        print("You're a child")
+        
+    elif age <= 18:
+        print("You're a teen")
+        
+    elif age <= 60:
+        print("You're an adult")
+        
+    else:
+        print("You're a senior citizen")
+        
+else:
+    print("Please enter a valid integer greater than Zero")
+
+    
