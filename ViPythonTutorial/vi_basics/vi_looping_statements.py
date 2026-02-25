5d173dfdc64a78f335469a02265889cabeab0368
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-02-25 08:52:58 +0530

    25-02-26

---
+++ ViPythonTutorial/vi_basics/vi_looping_statements.py
@@ -0,0 +1,29 @@
+'''
+Created on 25-Feb-2026
+
+@author: Vivek
+
+Looping statements(LS): LS are used to execute any statements repeatedly
+
+A loop will be executed repeatedly until a condition is fulfilled
+
+Types of LS:
+
+1. While loop:
+    a. Initial variable
+    b. Condition
+    c. Increment/ decrement
+2. For loop
+
+'''
+
+count = 0
+
+while count < 5 :
+    print("My name is Vivek")
+    count = count + 1
+
+
+
+
+
