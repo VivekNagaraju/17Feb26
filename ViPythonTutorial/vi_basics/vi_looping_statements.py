d1e44f707e91969fd30720ab258726f3ce2afc3c
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-02-26 08:46:58 +0530

    26-02-26

--- ViPythonTutorial/vi_basics/vi_looping_statements.py
+++ ViPythonTutorial/vi_basics/vi_looping_statements.py
@@ -15,15 +15,50 @@ Types of LS:
     c. Increment/ decrement
 2. For loop
 
+Infinite Loop: Loop never stops execution
+
 '''
+'''
+count = 0 # Variable initialization
+
+while count < 5 : # Defining condition
+    print("My name is Vivek")
+    # count = count + 1 # Increment
+    count += 1
+'''
+'''
+# Infinite loop creation:
+
+# Case 1:
+
+count = 5
+
+while count > 0:
+    print("My name is Vivek")
+    count += 1
+    
+# Case 2:
 
 count = 0
 
-while count < 5 :
+while count < 5:
     print("My name is Vivek")
-    count = count + 1
+    count -= 1
 
+# Case 3: Intentionally created
 
+while True:
+    print("My name is bharath")
+    
+'''
+'''
+# No Output
+count = 5
+while count > 5:
+    print("My name is bharath")
+    # count = count + 1 # Increment
+    count = count + 1    
+'''
 
 
 
