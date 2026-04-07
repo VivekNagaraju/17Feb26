00d2f831a67a9cd637102f9cf4cd81367f8b52f3
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-04-07 08:54:46 +0530

    07-04-26

---
+++ ViPythonTutorial/vi_oops/__init__.py
@@ -0,0 +1,10 @@
+'''
+OOPS: Object Oriented Programming Systems
+
+Programming Paradigms - style of programming
+
+Object: Real-world entities
+    - physical appearance - properties/attributes
+    - functionality
+
+'''
