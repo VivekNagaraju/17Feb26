86b7160e0ddef5759c39c182c3e158ee37ad8057
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-04-08 08:39:17 +0530

    08-04-26

--- ViPythonTutorial/vi_oops/__init__.py
+++ ViPythonTutorial/vi_oops/__init__.py
@@ -3,8 +3,54 @@ OOPS: Object Oriented Programming Systems
 
 Programming Paradigms - style of programming
 
-Object: Real-world entities
-    - physical appearance - properties/attributes
+Object: Real-world entities(features + functionalities)
+    - physical appearance - properties/attributes/features
     - functionality
+
+obj mobile1():    
+    name = "Samsung"
+    ram = 2
+    rom = 16
+    camera = 16
+    
+    def call():
+        print("Calling")
+    
+    def video_calling():
+        print("Video calling started")
+        
+obj mobile2():    
+    name = "Samsung"
+    ram = 6
+    rom = 256
+    camera = 20
+    
+    def call():
+        print("Calling")
+        
+    def video_calling():
+        print("Video calling started")
+        
+
+
+
+
+class mobile():
+    name 
+    ram 
+    rom
+    camera 
+    
+    def call():
+        print("Calling")
+        
+    def video_calling():
+        print("Video calling started")
+
+
+
+
+
+
 
 '''
