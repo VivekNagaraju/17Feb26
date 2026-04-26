e3dd9cf0bbeed4e59461a8d5e6a42ca9fa01ffca
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-04-26 18:02:41 +0530

    26-04-26

--- ViSeleniumTutorial/vi_selenium_basics/vi_frames.py
+++ ViSeleniumTutorial/vi_selenium_basics/vi_frames.py
@@ -1,5 +1,5 @@
 '''
-Created on 25-Apr-2026
+Created on 26-Apr-2026
 
 @author: Vivek
 '''
@@ -19,16 +19,11 @@ driver = webdriver.Chrome(options)
 driver.implicitly_wait(20)
 
 '''2. Navigating to a web page'''
-driver.get("https://testautomationpractice.blogspot.com/")
+driver.get("https://demo.automationtesting.in/Frames.html")
 
-'''3. Click on Simple Alert button'''
-simple_alert_btn = driver.find_element(By.ID, "alertBtn")
-simple_alert_btn.click()
+'''3. Switch to the iFrame'''
+driver.switch_to.frame('singleframe')
 
-'''4. Switch to the alert popup'''
-alert_popup = driver.switch_to.alert
-
-time.sleep(2)
-
-'''5. Click on OK in the alert present'''
-alert_popup.accept()
+'''4. Enter text in text field inside frame'''
+input_single_frame = driver.find_element(By.TAG_NAME, 'input')
+input_single_frame.send_keys("Vivek")
