476535b6b2a07282928ad98cf88008644e5d1108
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-06-04 08:46:01 +0530

    04-06-26

--- ViSeleniumTutorial/vi_pyunit/vi_unittest_setup_teardown.py
+++ ViSeleniumTutorial/vi_pyunit/vi_unittest_setup_teardown.py
@@ -1,48 +1,39 @@
 '''
-Created on 31-May-2026
+Created on 04-Jun-2026
 
-@author: Vivek
-
-PyUnit Features:
-    Assertions: functions for validations
-    Test Report
+@author: admin
 '''
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestLoginPage(unittest.TestCase):
+class TestLoginPageST(unittest.TestCase):
 
 
-    def test_navigate_to_login_page(self):
+    def setUp(self, driver):
         # 1. Launch Chrome browser
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
         options.add_argument("start-maximized")
         
-        driver = webdriver.Chrome(options)
+        self.driver = webdriver.Chrome(options)
         driver.implicitly_wait(10)
         
         # 2. Navigate to OrangeHRM Login page
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_navigate_to_login_page(self, driver):        
         # 3. Validate whether "login" is present in Current page URL
         current_page_url = driver.current_url
         self.assertIn("login", current_page_url, "Navigation failed")
     
-    def test_login_to_orangehrm(self):
-        # 1. Launch Chrome browser
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("detach", True)
-        options.add_argument("start-maximized")
-        
-        driver = webdriver.Chrome(options)
-        driver.implicitly_wait(10)
-        
-        # 2. Navigate to OrangeHRM Login page
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        
+    def test_login_to_orangehrm(self, driver):        
         # 3. Enter Username
         user_wrt = driver.find_element(By.NAME, 'username')
         user_wrt.send_keys('admin')
@@ -58,7 +49,6 @@ class TestLoginPage(unittest.TestCase):
         # 6. Validate whether "dashboard" is presnt in Current page URL
         current_page_url = driver.current_url
         self.assertIn("dashboard", current_page_url, "Login Failed")
-            
 
 
 if __name__ == "__main__":
